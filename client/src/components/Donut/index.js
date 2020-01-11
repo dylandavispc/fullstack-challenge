fef7af238a38fdcd8584a9f8a2c4e263bf9729ed
Dylan Davis <dylandavispc@gmail.com>
2020-01-11 10:53:41 -0500

    Donut problems solved; App MVP Full Functional

--- client/src/components/Donut/index.js
+++ client/src/components/Donut/index.js
@@ -2,27 +2,27 @@ import React, { Component } from "react";
 import {Doughnut} from 'react-chartjs-2';
 import "./style.css";
 
-const data = {
+// const data = {
 
-	labels: ["yo", "dog", "yo"],
-	datasets: [{
-		data: [20, 40, 20],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-        '#FFCE56',
-        '#FF6384',
-		'#36A2EB',
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-        '#FFCE56',
-        '#FF6384',
-		'#36A2EB',
-		]
-	}]
-};
+// 	labels: [],
+// 	datasets: [{
+// 		data: [],
+// 		backgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+//         '#FFCE56',
+//         '#FF6384',
+// 		'#36A2EB',
+// 		],
+// 		hoverBackgroundColor: [
+// 		'#FF6384',
+// 		'#36A2EB',
+//         '#FFCE56',
+//         '#FF6384',
+// 		'#36A2EB',
+// 		]
+// 	}]
+// };
 
 const options = {
     legend: {
@@ -34,39 +34,68 @@ export default class Donut extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loaded: false
+            names: [],
+            parts: []
         };
+        this.op = this.op.bind(this);
         this.fillChart = this.fillChart.bind(this);
     };
 
     // Functions
-    componentDidUpdate = () => {
-        if (this.state.loaded === false) {
-            this.fillChart();
-        };
-    };
+    // componentDidUpdate = () => {
+    //     this.fillChart();
+    // };
 
-    fillChart= () => {
-        this.setState({
-            loaded: true
-        })
+    op = (names, parts) => {
+        // console.log(names)
+        // console.log(parts)
+        const data = {
+            labels: names,
+            datasets: [{
+                data: parts,
+                backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56',
+                '#FF6384',
+                '#36A2EB',
+                ],
+                hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56',
+                '#FF6384',
+                '#36A2EB',
+                ]
+            }]
+        };
+        return data;
+    }
+
+    fillChart = () => {
         // console.log("form", this.props.parts);
-        const props = this.props.parts;
-        console.log(props)
-        console.log()
-        // this.props.parts.map((x) => {
-        //     const { id, firstName, lastName, part } = x;
-        //         console.log(data.datasets.data)
-        //         data.datasets.data.push(part);
-        //         data.labels.push(firstName + " " + lastName);
-        // });
+        const proper = this.props.parts;
+        let names = [];
+        let parts = [];
+        // console.log(data.labels)
+        // console.log(data.datasets[0].data)
+        this.props.parts.map((x) => {
+            const { firstName, lastName, part } = x;
+            let name = firstName + " " + lastName;
+            names.push(name);
+            parts.push(part);
+        });
+        const chart = this.op(names, parts);
+        console.log(chart)
+        return chart;
     };
+    
 
     // Render
     render() {
         return (
             <div>
-                <Doughnut data={data} options={options} />
+                <Doughnut data={this.fillChart()} options={options} />
             </div>
         );
     };
