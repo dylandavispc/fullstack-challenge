6232c30bc590a4df15e906fae117a12305065130
Dylan Davis <dylandavispc@gmail.com>
2020-01-12 12:16:35 -0500

    yoy

---
+++ client/build/precache-manifest.5e2292d0722631a0e74e8145b432051f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7dd74d8da65b598ee14d",
+    "url": "/static/js/main.ca659768.chunk.js"
+  },
+  {
+    "revision": "669813ff31679cdeb53d",
+    "url": "/static/js/2.f3028eea.chunk.js"
+  },
+  {
+    "revision": "7dd74d8da65b598ee14d",
+    "url": "/static/css/main.6c3502ec.chunk.css"
+  },
+  {
+    "revision": "4f5694e7a21e7bb734867de725fe404a",
+    "url": "/index.html"
+  }
+];
